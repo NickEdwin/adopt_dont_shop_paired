5f30e4b27016b59159dbfa0e7e56ed3e438a2356
Gaby Mendez <gmendez90@gmail.com>
2020-07-06 21:41:12 -0400

    Edit spacing on spec, include coverage inside .gitignore

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -16,11 +16,11 @@ RSpec.describe "visit /shelters/id", type: :feature do
   end
 
   it "displayes a list of reviews including title, rating, content, and optional picture" do
+    
     shelter1 = Shelter.create!({name: "Shelter 1", address: "1234 NW 10th St.", city: "Gainesville", state: "FL", zip: 32609})
     review1 = Review.create!({title: "My great experience!", rating: 5, content: "I went here and loved it.", picture: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12234558/Chinook-On-White-03.jpg", shelter_id: shelter1.id})
 
     visit "/shelters/#{shelter1.id}"
-
 
     expect(page).to have_content(review1.title)
     expect(page).to have_content(review1.rating)
