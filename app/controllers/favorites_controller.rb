3a38ae005eafd29d59ee0014bb0e454b472886f8
Gaby Mendez <gmendez90@gmail.com>
2020-07-09 16:05:50 -0400

    Delete comments, clean up code

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,12 +7,6 @@ class FavoritesController < ApplicationController
       end
     else
     end
-
-
-
-
-    # @fav_images = session[:fav_pets_images]
-    # @fav_ids = session[:fav_pets_ids]
   end
 
   def update
@@ -21,18 +15,5 @@ class FavoritesController < ApplicationController
     session[:fav_pets] ||= []
     session[:fav_pets] << @pet.id
     redirect_to "/pets/#{@pet.id}"
-
-
-    # session[:fav_pets_images] ||= Hash.new
-    # session[:fav_pets_images][@pet.name] = @pet.image
-    # session[:fav_pets_ids] ||= Hash.new
-    # session[:fav_pets_ids][@pet.name] = @pet.id
   end
 end
-
-# --- for future refactoring ---
-# session[:fav_pets] = [3, 54, 2] (<~ store the ids in an array)
-# @current_pet = AR.find pet[:id] (<~ use the ids in an AR search to locate pet)
-# @current_pet.name (<~ then we have ac cess to these things)
-# @current_pet.image
-# @current_pet.id
