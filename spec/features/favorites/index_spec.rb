e02a2b88179f2ed1091190647b01d6c949943585
Nick Edwin <nickedwin85@gmail.com>
2020-07-10 17:54:30 -0600

    updated test for adoption submission form

--- spec/features/favorites/index_spec.rb
+++ spec/features/favorites/index_spec.rb
@@ -2,6 +2,7 @@ RSpec.describe "as a visitor", type: :feature do
   before :each do
     @shelter = Shelter.create(name: "Braun Farm")
     @pet1 = Pet.create(name: 'Noodle', approx_age: 3, sex: "male", description: "description of noodle", image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001403/Australian-Cattle-Dog-On-White-03.jpg", shelter_id: @shelter.id, status: "adoptable" )
+    @pet2 = Pet.create(name: 'Cat Face', approx_age: 4, sex: "female", description: "This is a cat.", image: "https://static.toiimg.com/photo/msid-67586673/67586673.jpg?3918697", shelter_id: @shelter.id, status: "adoptable" )
 
   end
 
@@ -64,7 +65,43 @@ RSpec.describe "as a visitor", type: :feature do
       expect(current_path).to eq("/favorites")
       expect(page).to_not have_content("Noodle")
       expect(page).to_not have_content("Yoda")
+    end
 
+    it 'shows option to adopt my favorited pets' do
+
+      visit "/pets/#{@pet1.id}"
+      click_on 'Add pet to favorites'
+      visit "/pets/#{@pet2.id}"
+      click_on 'Add pet to favorites'
+      visit '/favorites'
+
+      expect(page).to have_link("Adopt favorited pets")
+
+      click_on 'Adopt favorited pets'
+
+      expect(current_path).to eq("/favorites/adoption/new")
+
+      expect(page).to have_content("#{pet1.name}")
+      expect(page).to have_content("#{pet2.name}")
+      check "#{pet1.name}"
+      check "#{pet2.name}"
+
+      fill_in 'Name', with: 'Timmy'
+      fill_in 'Address', with: '123 Street St.'
+      fill_in 'City', with: 'Denver'
+      fill_in 'State', with: 'CO'
+      fill_in 'Zip', with: '80218'
+      fill_in 'Phone Number', with: '303-123-4567'
+      fill_in 'Description', with: 'Because I love animals!'
+
+      click_on 'Submit Application'
+      expect(page).to have_content("Your application has been submitted.")
+
+      expect(current_path).to eq("/favorites")
+
+      expect(page).to_not have_content("#{pet1.name}")
+      expect(page).to_not have_content("#{pet2.name}")
+      expect(page).to have_content("You have no favorite pets.")
     end
   end
 end
