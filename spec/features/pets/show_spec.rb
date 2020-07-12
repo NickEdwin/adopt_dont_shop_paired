c2477a07b56a62dad786d62dab14d4609b8751a3
Nick Edwin <nickedwin85@gmail.com>
2020-07-12 15:46:10 -0600

    Updated test to show when a pet has no applicants

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -2,6 +2,7 @@ RSpec.describe 'as a visitor' do
   before :each do
     @shelter = Shelter.create(name: "Braun Farm")
     @pet1 = Pet.create(name: 'Noodle', approx_age: 3, sex: "male", description: "description of noodle", image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001403/Australian-Cattle-Dog-On-White-03.jpg", shelter_id: @shelter.id, status: "adoptable" )
+    @pet2 = Pet.create(name: 'Noodle', approx_age: 3, sex: "male", description: "description of noodle", image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001403/Australian-Cattle-Dog-On-White-03.jpg", shelter_id: @shelter.id, status: "adoptable" )
     @application1 = Application.create(name: 'Timmy', address: '123 Street St.', city: 'Denver', state: 'CO', zip: '80218', phone_number: '303-123-4567', reason: 'Because I love animals!')
     ApplicationPet.create(application_id: @application1.id, pet_id: @pet1.id)
   end
@@ -55,5 +56,11 @@ RSpec.describe 'as a visitor' do
     click_on "Timmy"
 
     expect(current_path).to eq("/applications/#{@application1.id}")
+  end
+
+  it 'Displays that a pet has no applicants' do
+    visit "/pets/#{@pet2.id}"
+
+    expect(page).to have_content("There are no current applications for this pet.")
   end
 end
