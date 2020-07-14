595ec3e399056e5e789a8f6b0aa8c8e820330d2b
Gaby Mendez <gmendez90@gmail.com>
2020-07-13 21:33:03 -0400

    Remove status from db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@
 shelter = Shelter.create(name: "Braun Farm", address: '4242 Farm Rd.', city: 'Eustis', state: 'FL', zip: 33790)
 shelter2 = Shelter.create(name: "Jax Rescue", address: '30 Matanzas Way.', city: 'Jacksonville', state: 'FL', zip: 33578)
 
-pet1 = Pet.create(name: 'Noodle', approx_age: 3, sex: "male", image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001403/Australian-Cattle-Dog-On-White-03.jpg", shelter_id: shelter.id, status: 'adoptable' )
+pet1 = Pet.create(name: 'Noodle', approx_age: 3, sex: "male", image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001403/Australian-Cattle-Dog-On-White-03.jpg", shelter_id: shelter.id)
 
-pet2 = Pet.create(name: 'Marley', approx_age: 12, sex: "female", image: "https://s3-eu-west-1.amazonaws.com/w3.cdn.gpd/gb.pedigree.56/large_e6bfdad9-6951-407b-a11f-bbd0c25bd796.jpg", shelter_id: shelter.id, status: 'adoptable' )
+pet2 = Pet.create(name: 'Marley', approx_age: 12, sex: "female", image: "https://s3-eu-west-1.amazonaws.com/w3.cdn.gpd/gb.pedigree.56/large_e6bfdad9-6951-407b-a11f-bbd0c25bd796.jpg", shelter_id: shelter.id)
 
-pet3 = Pet.create(name: 'Leo', approx_age: 7, sex: 'male', image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12235957/Border-Collie-On-White-01.jpg", shelter_id: shelter2.id, status: 'adoptable')
+pet3 = Pet.create(name: 'Leo', approx_age: 7, sex: 'male', image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12235957/Border-Collie-On-White-01.jpg", shelter_id: shelter2.id)
