d4191db5a0e7aadad86519806b866b92721461cd
Gaby Mendez <gmendez90@gmail.com>
2020-07-14 16:50:20 -0400

    Add spec to pets#edit to account for flash messages

--- spec/features/pets/edit_spec.rb
+++ spec/features/pets/edit_spec.rb
@@ -1,17 +1,34 @@
 RSpec.describe "as a visitor", type: :feature do
+  before :each do
+    @shelter = Shelter.create!({name: "Braun Farm", address: "1234 NW 10th St.", city: "Gainesville", state: "FL", zip: 32609})
+
+    @pet1 = Pet.create!(name: 'Noodle', approx_age: 3, sex: "male", description: "description of noodle", image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001403/Australian-Cattle-Dog-On-White-03.jpg", shelter_id: @shelter.id)
+  end
+
   it "uses 'update pet' link on pet show page to edit image, name, description, approx age, and sex with a form" do
 
-    shelter = Shelter.create!({name: "Braun Farm", address: "1234 NW 10th St.", city: "Gainesville", state: "FL", zip: 32609})
-
-    pet1 = Pet.create!(name: 'Noodle', approx_age: 3, sex: "male", description: "description of noodle", image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001403/Australian-Cattle-Dog-On-White-03.jpg", shelter_id: shelter.id)
-
-    visit "/pets/#{pet1.id}"
+    visit "/pets/#{@pet1.id}"
     click_on "Update"
-    expect(current_path).to eq("/pets/#{pet1.id}/edit")
+    expect(current_path).to eq("/pets/#{@pet1.id}/edit")
 
     fill_in('name', with: 'Noodle Mendez')
     click_on('Save')
-    expect(current_path).to eq("/pets/#{pet1.id}")
-    page.has_content?('Noodle Mendez')
+    expect(current_path).to eq("/pets/#{@pet1.id}")
+    expect(page).to have_content('Noodle Mendez')
+  end
+
+  it 'shows flash message when user doesn\'t fill all fields' do
+
+    visit "/pets/#{@pet1.id}"
+    click_on "Update"
+
+    expect(current_path).to eq("/pets/#{@pet1.id}/edit")
+
+    fill_in('name', with: nil)
+    click_on('Save')
+
+    expect(current_path).to eq("/pets/#{@pet1.id}/edit")
+    expect(page).to have_content('Name can\'t be blank')
+
   end
 end
